17c450bc659c9bc69378c037d9214a3eb72335f3
qxz1ftn <rui.monte@ctw.bmwgroup.com>
2026-06-09 15:43:33 +0100

    feat: update Firebase configuration in environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: false,
   // Replace with your Firebase project configuration.
   firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ''
+  apiKey: "AIzaSyDWA2VFYj5TteDD-9mOo3lY9P23EMjiQfw",
+  authDomain: "cdr-escala.firebaseapp.com",
+  projectId: "cdr-escala",
+  storageBucket: "cdr-escala.firebasestorage.app",
+  messagingSenderId: "178262706636",
+  appId: "1:178262706636:web:8aab3ca443c2eeb80b7569"
   }
 };
